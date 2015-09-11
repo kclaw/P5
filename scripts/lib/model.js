ae36a5337904c37d42ed0e24980fff3ff86904ab
kwanchi <kwanc.law@gmail.com>
2015-09-12 00:52:33 +0800

    add model

--- scripts/lib/model.js
+++ scripts/lib/model.js
@@ -9,6 +9,15 @@ define('model',[],function(){
                   'name':'school',
                 'latitude': 22.4518,
                     'longitude': 114.13
+                },{
+                    'name':'university',
+                    'latitude': 22.4521,
+                    'longitude': 114.152
+                },{
+                    'name':'peter\'shome',
+                    'latitude' : 22.461,
+                    'longitude' : 114.161
+
                 }
             ]
      };
